57ef867f1a2071bffaba4d1060c25087180fda51
Gunjan-Ahuja <gunjan2892@outlook.com>
2019-06-25 19:01:44 +0530

    initial commit

---
+++ src/main/java/springb/Sp.java
@@ -0,0 +1,15 @@
+package springb;
+
+public class Sp {
+private String name;
+
+public String getName() {
+	System.out.println("name is:" + name);
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+}
